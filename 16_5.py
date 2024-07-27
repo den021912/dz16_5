4694d8a3a98447366e08c68ed02b57be3aa6e668
den021912 <usakovdenis440@gmail.com>
2024-07-27 14:49:52 +0400

    Add files via upload

---
+++ 16_5.py
@@ -0,0 +1,54 @@
+from fastapi import FastAPI, status, Body, HTTPException, Request, Form
+from fastapi.responses import HTMLResponse
+from pydantic import BaseModel
+from fastapi.templating import Jinja2Templates
+
+app = FastAPI()
+templates = Jinja2Templates(directory = "templates")
+users = []
+
+class User(BaseModel):
+    text: str
+    id: int = None
+
+@app.get('/')
+def get_all_messages(request: Request) -> HTMLResponse:
+    return templates.TemplateResponse("users.html", {"request": request, "text": users})
+
+@app.get("/users/{user_id}")
+def get_message(request: Request, user_id: int) -> HTMLResponse:
+    try:
+        return templates.TemplateResponse("users.html", {"request": request, "text": users[user_id]})
+    except IndexError:
+        raise HTTPException(status_code=404, detail='Message not found')
+
+@app.post('/')
+def create_message(request: Request, text: str = Form()) -> HTMLResponse:
+    if users:
+        user_id = max(users, key = lambda u: u.id).id + 1
+    else:
+        user_id = 0
+    users.append(User(id = user_id, text = text))
+    return templates.TemplateResponse("users.html", {"request": request, "text": users})
+
+# @app.put("/message/{message_id}")
+# def update_message(message_id: int, message: str = Body()) -> str:
+#     try:
+#         edit_message = messages_db[message_id]
+#         edit_message.text = message
+#         return f'Message updated'
+#     except IndexError:
+#         raise HTTPException(status_code=404, detail='Message not found')
+#
+# @app.delete("/message/{message_id}")
+# def delete_message(message_id: int) -> str:
+#     try:
+#         messages_db.pop(message_id)
+#         return f"Message ID = {message_id} deleted"
+#     except IndexError:
+#         raise HTTPException(status_code=404, detail='Message not found')
+#
+# @app.delete("/")
+# def kill_messages_all() -> str:
+#     messages_db.clear()
+#     return 'All Messages deleted'
