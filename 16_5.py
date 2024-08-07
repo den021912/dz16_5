83804d02f83b434d4cf76b7a4d1f2f0d0be39249
den021912 <usakovdenis440@gmail.com>
2024-08-07 14:09:28 +0400

    Update 16_5.py

--- 16_5.py
+++ 16_5.py
@@ -3,35 +3,65 @@ from fastapi.responses import HTMLResponse
 from pydantic import BaseModel
 from fastapi.templating import Jinja2Templates
 
-app = FastAPI()
-templates = Jinja2Templates(directory = "templates")
+app = FastAPI(swagger_ui_parameters={"tryItOutEnabled": True}, debug=True)
+templates = Jinja2Templates(directory='templates')
 users = []
 
 class User(BaseModel):
-    text: str
-    id: int = None
-
+    id: int
+    username: str
+    age: int
 
 
 @app.get('/')
-def get_all_messages(request: Request) -> HTMLResponse:
-    return templates.TemplateResponse("users.html", {"request": request, "text": users})
+async def get_main_page(request: Request) -> HTMLResponse:
+    return templates.TemplateResponse('users.html', {'request': request, 'users': users})
 
-@app.get("/users/{user_id}")
-def get_message(request: Request, user_id: int) -> HTMLResponse:
-    try:
-        return templates.TemplateResponse("users.html", {"request": request, "text": users[user_id]})
-    except IndexError:
-        raise HTTPException(status_code=404, detail='Message not found')
 
-@app.post('/')
-def create_user(username:str, age:int):
-    username = username
-    age = age
-def create_message(request: Request, text: str = Form()) -> HTMLResponse:
-    if users:
-        user_id = max(users, key = lambda u: u.id).id + 1
-    else:
-        user_id = 0
-    users.append(User(id = user_id, text = text))
-    return templates.TemplateResponse("users.html", {"request": request, "text": users})
+@app.get('/user/{user_id}')
+async def get_users(
+    request: Request,
+    user_id: Annotated[int, Path(ge=1, le=100, description='Enter User ID', example='1')]
+) -> HTMLResponse:
+    for user in users:
+        if int(user.id) == user_id:
+            return templates.TemplateResponse('users.html', {'request': request, 'user': user})
+    raise HTTPException(status_code=404, detail='User was not found')
+
+
+@app.post('/user/{username}/{age}')
+async def post_user(
+        username: Annotated[str, Path(min_length=5, max_length=20,
+                                      description='Enter username', example='UrbanUser')],
+        age: Annotated[int, Path(le=120, ge=18, description='Enter age', example='24')]
+) -> User:
+    user_id = max(users, key=lambda x: int(x.id)).id + 1 if users else 1
+    user = User(id=user_id, username=username, age=age)
+    users.append(user)
+    return user
+
+
+@app.put('/user/{user_id}/{username}/{age}')
+async def update_user(
+        user_id: Annotated[int, Path(ge=1, le=100, description='Enter User ID', example='1')],
+        username: Annotated[str, Path(min_length=5, max_length=20,
+                                      description='Enter username', example='UrbanUser')],
+        age: Annotated[int, Path(le=120, ge=18, description='Enter age', example='24')]
+) -> User:
+    for user in users:
+        if user.id == user_id:
+            user.username = username
+            user.age = age
+            return user
+    raise HTTPException(status_code=404, detail='User was not found')
+
+
+@app.delete('/user/{user_id}')
+async def delete_user(
+        user_id: Annotated[int, Path(ge=1, le=100, description='Enter User ID', example='1')],
+) -> User:
+    for user in users:
+        if user.id == user_id:
+            users.remove(user)
+            return user
+    raise HTTPException(status_code=404, detail='User was not found')
